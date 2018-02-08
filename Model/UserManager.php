8b1ca06fe1159076b7cad8b44f55f44aa986400f
Yanis Bendahmane <yanis.bendahmane@supinternet.fr>
2018-02-08 18:43:58 +0100

    User adding into database and hashing the password

---
+++ Model/UserManager.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('Cool/DBManager.php');
+
+class UserManager
+{
+    public function registerUser($user, $firstName, $lastName, $email, $password, $passwordVerify)
+    {
+        if(isset($user) && isset($firstName)
+        && isset($lastName) && isset($email)
+        && isset($password) && isset($passwordVerify)){
+            if($password === $passwordVerify){
+                $hashedPwd = hash(md5, $password);
+                $dbm = DBManager::getInstance();
+                $pdo = $dbm->getPdo();
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                $stmt = $pdo->prepare("INSERT INTO `Users` (`id`, `username`, `firstName`, `lastName`, `email`, `password`) 
+                VALUES (NULL, :user, :firstName, :lastName, :email, :psw)");
+                $stmt->bindParam(':user', $user);
+                $stmt->bindParam(':firstName', $firstName);
+                $stmt->bindParam(':lastName', $lastName);
+                $stmt->bindParam(':email', $email);
+                $stmt->bindParam(':psw', $hashedPwd);
+
+                $stmt->execute();
+            }
+        }
+    }
+}
