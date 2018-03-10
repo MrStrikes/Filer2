34e935e9bfc07f6227a630c4b4edc2f8873e5f25
MrStrikes <yanis.bendahmane@supinternet.fr>
2018-03-10 18:15:43 +0100

    Added security for non-logged in users

--- Controller/UploadController.php
+++ Controller/UploadController.php
@@ -8,49 +8,57 @@ class UploadController extends BaseController
 {
     public function uploadAction()
     {
-        $manager = new FilesManager();
-        if (isset($_POST['remove'])) {
-            $file = $_POST['hiddenFile'];
-            $manager->removeFile($file);
-            $logs = fopen('logs/access.log', 'a+');
-            fwrite($logs, $_SESSION['username'].' just deleted the file '.$file."\n");
-            fclose($logs);
-            return $this->redirect('?action=upload');
-        } else if (isset($_POST['download'])) {
-            $file = $_POST['hiddenDownloadFile'];
-            $manager = new FilesManager();
-            $manager->downloadFile($file);
-            $logs = fopen('logs/access.log', 'a+');
-            fwrite($logs, $_SESSION['username'].' just downloaded the file '.$file."\n");
-            fclose($logs);
-            return $this->redirect('?action=upload');
+        if(empty($_SESSION['username'])){
+            return $this->redirect('?action=home');
         } else {
             $manager = new FilesManager();
-            $sendUserFiles = $manager->uploadFile();
-            $arr = [
-                'files' => $sendUserFiles,
-                'uploaddir' => 'upload/'.$_SESSION['username']
-            ];
-            return $this->render('upload.html.twig', $arr);
+            if (isset($_POST['remove'])) {
+                $file = $_POST['hiddenFile'];
+                $manager->removeFile($file);
+                $logs = fopen('logs/access.log', 'a+');
+                fwrite($logs, $_SESSION['username'].' just deleted the file '.$file."\n");
+                fclose($logs);
+                return $this->redirect('?action=upload');
+            } else if (isset($_POST['download'])) {
+                $file = $_POST['hiddenDownloadFile'];
+                $manager = new FilesManager();
+                $manager->downloadFile($file);
+                $logs = fopen('logs/access.log', 'a+');
+                fwrite($logs, $_SESSION['username'].' just downloaded the file '.$file."\n");
+                fclose($logs);
+                return $this->redirect('?action=upload');
+            } else {
+                $manager = new FilesManager();
+                $sendUserFiles = $manager->uploadFile();
+                $arr = [
+                    'files' => $sendUserFiles,
+                    'uploaddir' => 'upload/'.$_SESSION['username']
+                ];
+                return $this->render('upload.html.twig', $arr);
+            }
         }
     }
 
     public function renameAction()
     {
-        if(isset($_POST['oldName']) && isset($_POST['newName'])){
-            $oldName = $_POST['oldName'];
-            $newName = $_POST['newName'];
-            $manager = new FilesManager();
-            $manager->renameFile($oldName, $newName);
-            $logs = fopen('logs/access.log', 'a+');
-            fwrite($logs, $_SESSION['username'].' just renamed the file: \''.$oldName.'\' into: \''.$newName."'\n");
-            fclose($logs);
-            return $this->redirect('?action=upload');
+        if(empty($_SESSION['username'])){
+            return $this->redirect('?action=home');
         } else {
-            $arr = [
-                'oldName' => $_POST['hiddenFile']
-            ];
-            return $this->render('rename.html.twig', $arr);
+            if(isset($_POST['oldName']) && isset($_POST['newName'])){
+                $oldName = $_POST['oldName'];
+                $newName = $_POST['newName'];
+                $manager = new FilesManager();
+                $manager->renameFile($oldName, $newName);
+                $logs = fopen('logs/access.log', 'a+');
+                fwrite($logs, $_SESSION['username'].' just renamed the file: \''.$oldName.'\' into: \''.$newName."'\n");
+                fclose($logs);
+                return $this->redirect('?action=upload');
+            } else {
+                $arr = [
+                    'oldName' => $_POST['hiddenFile']
+                ];
+                return $this->render('rename.html.twig', $arr);
+            }
         }
     }
 }
